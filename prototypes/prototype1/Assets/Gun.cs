6a5e72f4a0c5b2df390396141f6f14417d5b7b3f
Liu WenYue <LiuWY0912@gmail.com>
2020-02-05 00:16:11 -0500

    P1: Finished prototype 1

---
+++ prototypes/prototype1/Assets/Gun.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+
+public class Gun : MonoBehaviour
+{
+    public float damage = 10f;
+    public float range = 50f;
+
+    public Camera fpsCam;
+    public ParticleSystem flash;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+		{
+            Shoot();
+		}
+    }
+
+    void Shoot()
+	{
+        flash.Play();
+
+        RaycastHit hit;
+
+        if (Physics.Raycast(fpsCam.transform.position, fpsCam.transform.forward, out hit, range))
+        {
+            Debug.Log(hit.transform.name);
+
+            Target target = hit.transform.GetComponent<Target>();
+            if (target != null)
+            {
+                target.TakeDamage(damage);
+            }
+		}
+
+
+    }
+}
