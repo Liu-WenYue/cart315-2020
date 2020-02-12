0ec250aaf4852e3c175c2f75f9aea6bb9d130c75
Liu WenYue <LiuWY0912@gmail.com>
2020-02-11 23:47:58 -0500

    P2: Finished Prototype 2

---
+++ prototypes/prototype2/Assets/Flying.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Flying : MonoBehaviour
+{
+    public float movementSpeed = 100f;
+    public float resetSpeed = 100f;
+    public float shiftSpeed = 150f;
+    public float controlSpeed = 50f;
+
+    public Animator camRotation;
+
+    void Update()
+    {
+        transform.position += Vector3.forward * Time.deltaTime * movementSpeed;
+
+        if (Input.GetKey(KeyCode.O))
+        {
+            camRotation.SetBool("Left", true);
+        }
+
+        if (Input.GetKey(KeyCode.P))
+        {
+            camRotation.SetBool("Right", true);
+        }
+
+        if (!Input.GetKey(KeyCode.O) && !Input.GetKey(KeyCode.P))
+        {
+            camRotation.SetBool("Fly", true);
+            camRotation.SetBool("Right", false);
+            camRotation.SetBool("Left", false);
+        }
+        else
+        {
+            camRotation.SetBool("Fly", false);
+        }
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.position += Vector3.up * Time.deltaTime * controlSpeed;
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.position += Vector3.left * Time.deltaTime * controlSpeed;
+        }
+
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.position += Vector3.down * Time.deltaTime * controlSpeed;
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.position += Vector3.right * Time.deltaTime * controlSpeed;
+        }
+
+    }
+}
---
+++ prototypes/prototype2/Assets/TriggerZone.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerZone : MonoBehaviour
+{
+    public bool isDamaging;
+    public float damage = 20f;
+
+    public void OnTriggerStay(Collider col)
+    {
+        if (col.tag == "Player")
+        {
+            col.SendMessage((isDamaging) ? "TakeDamage" : "HealthDamage", Time.deltaTime * damage);
+        }
+    }
+}
