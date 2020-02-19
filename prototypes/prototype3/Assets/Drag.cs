102525b987702333a6e7953e1961293ccb5cb94f
Liu WenYue <LiuWY0912@gmail.com>
2020-02-19 01:22:17 -0500

    P3: Finished Prototype 3

---
+++ prototypes/prototype3/Assets/Drag.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Drag : MonoBehaviour
+{
+    private Vector3 mouseOffset;
+    private float mouseZCoord;
+
+    void Start()
+    {
+        Cursor.visible = true;
+    }
+
+    void OnMouseDown()
+    {
+        mouseZCoord = Camera.main.WorldToScreenPoint(gameObject.transform.position).z;
+        mouseOffset = gameObject.transform.position - GetMouseWorldPos();
+    }
+
+    private Vector3 GetMouseWorldPos()
+    {
+        Vector3 mousePoint = Input.mousePosition;
+        mousePoint.z = mouseZCoord;
+
+        return Camera.main.ScreenToWorldPoint(mousePoint);
+    }
+
+    void OnMouseDrag()
+    {
+        transform.position = GetMouseWorldPos() + mouseOffset;
+    }
+
+    void Update()
+    {
+        Cursor.visible = true;
+    }
+}
